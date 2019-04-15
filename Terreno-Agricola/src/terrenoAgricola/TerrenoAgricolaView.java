435a091debba2336ae86b375a9583c3e5f52781b
Emanuel4168 <emanuel.l4168@gmail.com>
2019-04-15 11:21:37 -0600

    Creación de la vista principal

---
+++ Terreno-Agricola/src/terrenoAgricola/TerrenoAgricolaView.java
@@ -0,0 +1,27 @@
+package terrenoAgricola;
+
+import javax.swing.*;
+
+public class TerrenoAgricolaView extends JFrame{
+	
+	private JPanel centerPanel,southPanel;
+	
+	
+	public TerrenoAgricolaView() {
+		super("Terreno Agricola");
+		setSize(800,800);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+	
+	private void createCenterPane() {
+		
+	}
+	
+	private void createSouthPane() {
+		
+	}
+
+}
